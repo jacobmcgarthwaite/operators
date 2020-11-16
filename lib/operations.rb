3f8499969241e18d80a06290dc66761748d10db6
Jacob <jacobliem1@gmail.com>
2020-11-15 20:24:03 -0600

    commit tree 174494a6c341b953344b5136e2f371980f95adfd

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+    if speed > 60 || speed < 40
+        true
+    else speed >= 41 && speed <= 59
+        false
 end
-
-
+end
 
 def not_safe?(speed)
-	
+    speed > 60 || speed < 40 ? true : false
 end
-	
+
 
 
